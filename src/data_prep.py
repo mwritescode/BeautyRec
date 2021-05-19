5a5433adcda172d0572973770cfbe347d6a9e25b
Martina <martina.rossini704@gmail.com>
2021-05-19 20:29:33 +0200

    Adapted to the changes in scraping.py

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+import pickle
 
 from scraping import SephoraScraper
 
@@ -15,18 +16,19 @@ TO_SCRAPE = ['https://www.sephora.com/shop/moisturizer-skincare',
 LINKS_PATH = '../data/product_links.csv'
 
 def download_data(checkpoint=''):
-            scraper = SephoraScraper(driver_path=PATH)
-            if os.path.isfile(LINKS_PATH):
-                links = pd.read_csv(LINKS_PATH, sep='\t')['links'].to_list()
-                if len(checkpoint) > 0:
-                    scraper.load_checkpoint(checkpoint + '_products.csv', 
-                                        checkpoint + '_reviews.csv', links)
-            else:
-                links = scraper.scrape_links(TO_SCRAPE)
-                
-            scraper.scrape_products_and_reviews(num_pages_reviews=20, product_links=links)
-            scraper.save_products_as_csv('../data/products.csv')
-            scraper.save_ratings_as_csv('..data/ratings.csv')
+    if checkpoint:
+        with open(checkpoint, 'rb') as pickle_file:
+            scraper = pickle.load(pickle_file)
+    else:
+        scraper = SephoraScraper(driver_path=PATH)
+        if os.path.isfile(LINKS_PATH):
+            links = pd.read_csv(LINKS_PATH, sep='\t')['links'].to_list()
+        else:
+            links = scraper.scrape_links(TO_SCRAPE)
+
+    scraper.scrape_products_and_reviews(num_pages_reviews=2, product_links=links, checkpoint_after=2)
+    scraper.save_products_as_csv('../data/products.csv')
+    scraper.save_ratings_as_csv('..data/ratings.csv')
 
 def remove_nicknames(ratings):
     ratings['buyer_id'] = pd.factorize(ratings['buyer_nickname'])[0]
@@ -35,5 +37,3 @@ def remove_nicknames(ratings):
 
 
 download_data()
-#ratings = pd.read_csv('../data/moisturizers/ratings.csv', sep='\t')
-#remove_nicknames(ratings)
