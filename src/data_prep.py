efada6936b3bc533f8f1995211e4bc9e62c1e8f9
Martina <martina.rossini704@gmail.com>
2021-05-31 21:35:30 +0200

    Added code to compute the user-item matrix

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,10 +1,10 @@
 import os
 
 import pandas as pd
+import numpy as np
 import pickle
 
 from scraping import SephoraScraper
-from selenium import webdriver
 # System path to chrome webdriver and urls to scrape
 PATH = 'C:/WebDrivers/chromedriver.exe'
 
@@ -29,12 +29,19 @@ def download_data(checkpoint=''):
 
     scraper.scrape_products_and_reviews(num_pages_reviews=20, product_links=links, checkpoint_after=30, override=False)
     scraper.save_products_as_csv('../data/products.csv')
-    scraper.save_ratings_as_csv('..data/ratings.csv')
+    scraper.save_ratings_as_csv('../data/ratings.csv')
 
 def remove_nicknames(ratings):
     ratings['buyer_id'] = pd.factorize(ratings['buyer_nickname'])[0]
     ratings = ratings.drop(['buyer_nickname'], axis=1)
     return ratings
 
+def build_user_ratings_matrix(num_items, num_users, ratings):
+    matrix = np.zeros((num_users, num_items))
+    for _, row in ratings.iterrows():
+        matrix[row.buyer_id - 1, row.product_id -1] = row.rating
+    return matrix
 
-download_data('../data/16.pkl')
+#download_data('../data/27.pkl')
+#ratings = pd.read_csv('../data/ratings.csv', sep='\t')
+#ratings = remove_nicknames.ratings
