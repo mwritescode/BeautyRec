f8cb0070a9faa80010d53f22d74e48962949f12d
Martina <martina.rossini704@gmail.com>
2021-06-01 12:54:20 +0200

    Cleaned up the code a little

--- src/matrix_factorization.py
+++ src/matrix_factorization.py
@@ -9,22 +9,21 @@ from data_prep import remove_nicknames
 class MatrixFactorization():
     def __init__(self, ratings, num_users, num_items,  num_latent_factors=12):
         self.ratings = ratings
-        self._n_users = num_users
-        self._n_items = num_items
-        self.user_bias = np.zeros(self._n_users)
-        self.item_bias = np.zeros(self._n_items)
+        self.user_bias = np.zeros(num_users)
+        self.item_bias = np.zeros(num_items)
         self.global_bias = np.mean(self.ratings.rating)
-        self.U = np.random.random((self._n_users, num_latent_factors))
-        self.I = np.random.random((num_latent_factors, self._n_items))
+        self.U = np.random.random((num_users, num_latent_factors))
+        self.I = np.random.random((num_latent_factors, num_items))
         self.global_rmse = []
         self.validation_rmse = []
+        self.val_failures = 0
     
     def predict(self, user, item):
         pred = self.global_bias + self.user_bias[user] + self.item_bias[item]
         pred += self.U[user, :].dot(self.I[:, item])
         return pred
-
-    def train(self, max_iter=20, regularize=0.1, learning_rate=0.05, val=None):
+    
+    def _on_train_begins(self, val):
         self.global_rmse.append(self._compute_rmse(self.ratings))
         header_string = '{} \t | \t {} \t '.format('Iteration', 'RMSE')
         num_dashes = 40
@@ -32,26 +31,26 @@ class MatrixFactorization():
             header_string += ' \t | \t {}'.format('Validation RMSE')
             self.validation_rmse.append(self._compute_rmse(val))
             num_dashes = 70
-            fails = 0
         print(num_dashes*'-')
         print(header_string)
         print(num_dashes*'-')
-        iterations = max_iter
-        for i in range(max_iter):
+
+    def train(self, max_iter=20, regularize=0.1, learning_rate=0.05, val=None):
+        self._on_train_begins(val)
+        iteration = 0
+        while iteration < max_iter and self.val_failures < 3:
             self._sgd_step(regularize, learning_rate)
-            result_msg = '{:.0f} \t\t | \t {:.4f} '.format(i +1, self.global_rmse[-1])
+            result_msg = '{:.0f} \t\t | \t {:.4f} '.format(iteration +1, self.global_rmse[-1])
             if val is not None:
                 self.validation_rmse.append(self._compute_rmse(val))
                 result_msg += ' \t | \t {:.4f} '.format(self.validation_rmse[-1])
                 if self.validation_rmse[-1] >= self.validation_rmse[-2]:
-                    fails += 1
+                    self.val_failures += 1
                 else:
-                    fails = 0
+                    self.val_failures = 0
+            iteration += 1
             print(result_msg)
-            if val is not None and fails >= 3:
-                iterations = i + 1
-                break    
-        self._plot_rmse(iterations=range(iterations +1), val=val)
+        self._plot_rmse(iterations=range(iteration +1), val=val)
     
     def _sgd_step(self, regularize, learning_rate):
         ratings = self.ratings.sample(frac=1)
