1177a2d29ecc61d14b01d85c1bbec21c422ff952
Martina <martina.rossini704@gmail.com>
2021-06-01 12:53:56 +0200

    Added random seed for reproducibility

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-def train_test_split(data, test_prc=0.25):
+def train_test_split(data, test_prc=0.25, random_seeed=42):
     data_size = len(data)
     test_size = int(data_size * test_prc)
+    np.random.seed(random_seeed)
     test_indexes = np.random.randint(low=0, 
                                     high=data_size, 
                                     size=test_size)
