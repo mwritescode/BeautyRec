847a706d1d9da1a0d2912a2ff611f8982e19080f
Martina <martina.rossini704@gmail.com>
2021-06-03 19:34:32 +0200

    Fixed minor bugs

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,59 +1,17 @@
 import numpy as np
-from numpy.core.numeric import empty_like
+import pandas as pd
+from data_prep import remove_nicknames
+import matplotlib.pyplot as plt
 
-class DenseLayer():
-    def __init__(self, num_units, input_shape=None):
-        self.num_units = num_units
-        self.in_shape = (input_shape,)
-        self.inputs = None
-        self.W = None
-        self.bias = None
-        self.optimizer = None
-    
-    def initialize_params(self):
-        self.W = np.random.random((self.in_shape[0], self.num_units))
-        self.bias = np.random.random((1, self.num_units))
-    
-    def set_input_shape(self, shape):
-        self.in_shape = shape
-    
-    def get_output_shape(self):
-        return (self.num_units,)
-    
-    def compile(self, optimizer):
-        self.optimizer = optimizer
-    
-    def forward_pass(self, x, training=True):
-        self.inputs = x
-        out = np.dot(self.W, x) + self.bias
-        return out
-    
-    def backward_pass(self, accumulated_grad):
-        # Compute gradients
-        weights_grad = np.dot(self.inputs.T, accumulated_grad)
-        bias_grad = np.sum(accumulated_grad, axis=0, keepdims=True)
+import copy
 
-        accumulated_grad = np.dot(accumulated_grad, self.W.T)
+from layers import DenseLayer, LeakyReLU
+from optimizers import Adam, RMSProp
+from utils import SquareLoss
 
-        self.W = self.optimizer.update_weights(self.W, weights_grad)
-        self.bias = self.optimizer.update_bias(self.bias, bias_grad)
-
-        return accumulated_grad
-    
-    def to_string(self):
-        info_line = info_line = '|{:^26s}|{:^26s}|{:^26s}|'
-        input = 'Inputs: ({},)'.format(self.in_shape[0])
-        output = 'Outputs: ({},)'.format(self.num_units)
-        description = info_line.format(self.__class__.__name__,
-                                        input,
-                                        output)
-        return description 
-    
-
-class Model():
+class SequentialModel():
     def __init__(self, layers=None):
-        self.layers = layers
-        self._check_input_sizes()
+        self.layers = self._check_input_sizes(layers)
         self.optimizer = None
         self.loss = None
         self.errors = {
@@ -61,10 +19,13 @@ class Model():
             'validation': []
         }
     
-    def _check_input_sizes(self):
-        if self.layers:
-            for i, layer in enumerate(self.layers[1:]):
-                layer.set_input_shape(self.layers[i].get_output_shape())
+    def _check_input_sizes(self, layers):
+        if layers is not None:
+            for i, layer in enumerate(layers):
+                if i > 0:
+                    layer.set_input_shape(layers[i-1].get_output_shape())
+                layer.initialize_params()
+        return layers
     
     def add(self, layer):
         if self.layers:
@@ -75,19 +36,25 @@ class Model():
     def compile(self, optimizer, loss):
         self.optimizer = optimizer
         for layer in self.layers:
-            layer.compile(optimizer)
+            layer.compile(copy.deepcopy(optimizer))
         self.loss = loss
     
     def fit(self, x, y, num_epochs=100):
-        for _ in range(num_epochs):
+        for i in range(num_epochs):
             y_pred = self._forward_pass(x)
-            loss = self.loss.compute(y, y_pred)
+            loss = np.mean(self.loss.compute(y, y_pred))
             self.errors['training'].append(loss)
-            loss_grad = self.loss.grad(y. y_pred)
+            loss_grad = self.loss.grad(y, y_pred)
+            print('Epoch {} \t Loss: {} '.format(i, loss))
             self._backward_pass(loss_grad)
         return self.errors['training']
+    
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
-    def _forward_pass(self, x, training=True):
+    def _forward_pass(self, x):
         output = x
         for layer in self.layers:
             output = layer.forward_pass(output)
@@ -96,7 +63,6 @@ class Model():
     def _backward_pass(self, accumulated_grad):
         for layer in reversed(self.layers):
             accumulated_grad = layer.backward_pass(accumulated_grad)
-        return accumulated_grad
     
     def print_summary(self):
         print(' ' + 80*'-')
@@ -107,10 +73,33 @@ class Model():
             print(layer.to_string())
             print(empty_line)
             print(' ' + 80*'-')
+        
 
-layer1 = DenseLayer(num_units=128, input_shape=60)
-layer2 = DenseLayer(num_units=64)
-layer3 = DenseLayer(num_units=256)
+layer1 = DenseLayer(num_units=60, input_shape=40)
+#layer2 = DenseLayer(num_units=120)
+#layer3 = LeakyReLU()
+layer5 = DenseLayer(num_units=40)
 
-model = Model(layers=[layer1, layer2, layer3])
-model.print_summary()
+model = SequentialModel(layers=[layer1])
+model.add(layer5)
+#model.print_summary()
+optimizer = Adam(learning_rate=0.01)
+loss = SquareLoss()
+model.compile(optimizer=optimizer, loss=loss)
+x = np.linspace(0, 20, num=40)
+#noise = np.random.normal(3,1,500)
+y = (x ** 2)
+#x_test = np.linspace(0, 500, num=32)
+#y_test = np.sin(x_test)
+errors = model.fit(x, y, num_epochs=400)
+print('The final RMS is:', errors[-1])
+_, ax = plt.subplots()
+ax.plot(errors)
+plt.show()
+y_pred = model.predict(x)
+y_pred = y_pred.reshape(-1)
+_, ax = plt.subplots()
+ax.plot(x, y, label='Actual values',color='b')
+ax.plot(x, y_pred, label='Predicted values', color='r')
+plt.legend()
+plt.show()
